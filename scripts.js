dbab81c6b3cda50f00a31f0286614288976954c8
PaolaLima <lolabola2404@gmail.com>
2020-04-18 18:37:58 -0300

    criação da web

---
+++ scripts.js
@@ -0,0 +1,12 @@
+function on0ff(){
+  document
+.querySelector("#modal")
+.classList
+.toggle("hide")
+}
+
+//document
+//.querySelector("button.fat")
+//.addEventListener("click", on0ff)
+
+//adicionando um evento de clicke criando uma função de desaparecer o link//
