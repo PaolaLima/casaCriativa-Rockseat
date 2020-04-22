3de4346b726e707a13661ba3df2fef76fb0cf630
PaolaLima <lolabola2404@gmail.com>
2020-04-22 20:54:24 -0300

    alteração na modal, modificação na index e ideias, criação da função scroll

--- scripts.js
+++ scripts.js
@@ -2,11 +2,21 @@ function on0ff(){
   document
 .querySelector("#modal")
 .classList
-.toggle("hide")
-}
+.toggle("hide");
+};
 
 //document
 //.querySelector("button.fat")
-//.addEventListener("click", on0ff)
+//.addEventListener("click", on0ff);
 
 //adicionando um evento de clicke criando uma função de desaparecer o link//
+
+document
+.querySelector("#modal")
+.classList
+.toggle("hideScroll");
+
+document
+.querySelector("body")
+.classList
+.toggle("addScroll");
