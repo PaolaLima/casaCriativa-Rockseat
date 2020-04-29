13b8fc2df4734777cd13036d3461fce659c814a5
PaolaLima <lolabola2404@gmail.com>
2020-04-28 22:45:59 -0300

    back-end da aplicaçã

---
+++ server.js
@@ -0,0 +1,4 @@
+ const express = require("express"); //importando a blibioteca express
+ const server = express(); //atribuindo express na var server
+
+ server.listen(3000); //chamando a var server e atribuido o objeto listen
