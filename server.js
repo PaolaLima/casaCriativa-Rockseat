87336ba9fc8f80a949eb27ba523757a9f5f187d8
PaolaLima <lolabola2404@gmail.com>
2020-04-29 20:02:03 -0300

    criei uma rota, configurei arquivos estáticos

--- server.js
+++ server.js
@@ -1,4 +1,13 @@
  const express = require("express"); //importando a blibioteca express
  const server = express(); //atribuindo express na var server
 
- server.listen(3000); //chamando a var server e atribuido o objeto listen
+//configurar arquivos estáticos (css, scripts, imagens)
+server.use(express.static("public"));
+
+ //criei uma rota /
+//e capturo o pedido do cliente para responder
+server.get("/", function(req, res){
+    return res.sendFile(__dirname + "/index.html")
+});
+ 
+ server.listen(3000); //chamando a var server e atribuido a porta 3000 
